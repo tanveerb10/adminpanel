d8283df3334b09837c236877b828c974abd95d6b
tanveerb10 <tanveerbranded10@gmail.com>
2024-06-20 18:22:24 +0530

    admin user page

--- full-version/src/app/[lang]/(dashboard)/admin/adminusers/page.jsx
+++ full-version/src/app/[lang]/(dashboard)/admin/adminusers/page.jsx
@@ -1,7 +1,92 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from 'react'
 import Adminusers from '@/views/admin/adminusers/Adminusers'
-export default function page() {
-  return (
-    <div><Adminusers/></div>
-  )
+import Cookies from 'js-cookie'
+import CryptoJS from 'crypto-js'
+
+// Function to generate nonce
+const generateNonce = () => CryptoJS.lib.WordArray.random(16).toString()
+
+// Function to generate a timestamp
+const generateTimestamp = () => Date.now().toString()
+
+// Function to generate a signature
+const generateSignature = (payloaddata, secret, nonce, timestamp) => {
+  const payload = `${payloaddata}|${nonce}|${timestamp}`
+  return CryptoJS.HmacSHA256(payload, secret).toString(CryptoJS.enc.Hex)
 }
+
+// Function to get data using Fetch API
+const getData = async (setUserData, setError, setLoading) => {
+  const secret = process.env.NEXT_PUBLIC_SECRET_KEY
+  const token = Cookies.get('accessToken')
+
+  console.log({ token })
+
+  if (!secret) {
+    setError('Secret key is not defined')
+    setLoading(false)
+    return
+  }
+
+  if (!token) {
+    setError('Token is not defined')
+    setLoading(false)
+    return
+  }
+
+  const payloaddata = JSON.stringify({})
+  const nonce = generateNonce()
+  const timestamp = generateTimestamp()
+  const signature = generateSignature(payloaddata, secret, nonce, timestamp)
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_LIVE}/admin/admins`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'livein-key': 'livein-key',
+        Nonce: nonce,
+        Timestamp: timestamp,
+        Signature: signature,
+        Authorization: `Bearer ${token}` // Include the token in the Authorization header
+      }
+      // credentials: 'include',
+    })
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch userData, status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    setUserData(data)
+    console.log({ data })
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    setError(error.message)
+  } finally {
+    setLoading(false)
+  }
+}
+
+const Page = () => {
+  const [userData, setUserData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getData(setUserData, setError, setLoading)
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  return <Adminusers userData={userData} />
+}
+
+export default Page

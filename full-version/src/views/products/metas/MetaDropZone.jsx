4917fe8ca5510d3164b103f26489182405882280
tanveeeer00 <tanveer.shaikh@branded10.in>
2024-08-27 16:06:38 +0530

    metas bulk upload

---
+++ full-version/src/views/products/metas/MetaDropZone.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import AppReactDropzone from '@/libs/styles/AppReactDropzone'
+import { Button } from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const MetaDropzone = ({ onDrop, loading }) => {
+  const { getRootProps, getInputProps, acceptedFiles, isDragActive, open } = useDropzone({
+    multiple: false,
+
+    onDrop
+  })
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ))
+
+  return (
+    <>
+      <AppReactDropzone className='relative'>
+        {loading ? (
+          <div className='absolute bg-black/30 z-50 h-full w-full flex items-center justify-center cursor-not-allowed'>
+            <CircularProgress size={40} color='inherit' />
+          </div>
+        ) : null}
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <div className='text-center'>
+            {isDragActive ? (
+              <p>Release to drop the files here</p>
+            ) : (
+              <>
+                <p>Drag and drop CSV files here</p>
+                <p>OR</p>
+                <Button onClick={open}>Click to select file</Button>
+              </>
+            )}
+          </div>
+        </div>
+      </AppReactDropzone>
+    </>
+  )
+}
+
+export default MetaDropzone

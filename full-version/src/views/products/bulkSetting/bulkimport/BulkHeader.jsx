b2c0614b8b85c53fc29ae9f1e7b734fb1542ccf1
tanveeeer00 <tanveer.shaikh@branded10.in>
2024-09-06 09:59:51 +0530

    bulk history , export, table and minor changes

--- full-version/src/views/products/bulkSetting/bulkimport/BulkHeader.jsx
+++ full-version/src/views/products/bulkSetting/bulkimport/BulkHeader.jsx
@@ -4,9 +4,6 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
 export default function BulkHeader({ TabValue, handleExport, HeaderValue }) {
-  console.log(TabValue, 'form export')
-  console.log(['metasTab', 'productUpdateTab'].includes(TabValue), 'check yab value for export')
-  const includeExport = ['metasTab', 'productUpdateTab']
   return (
     <div className='flex items-center justify-between gap-6'>
       <div>

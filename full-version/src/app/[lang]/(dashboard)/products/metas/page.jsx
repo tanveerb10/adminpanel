8d1d4448a1b7161e8bd2fa337f0e4c885aa6eccf
tanveeeer00 <tanveer.shaikh@branded10.in>
2024-09-06 14:13:00 +0530

    starting point from creating table

--- full-version/src/app/[lang]/(dashboard)/products/metas/page.jsx
+++ full-version/src/app/[lang]/(dashboard)/products/metas/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-import Metas from '@/views/products/metas/Metas'
+'use client'
+import { useEffect, useState } from 'react'
+import Allproducts from '@/views/products/allproducts/Allproducts'
+import fetchData from '@/utils/fetchData'
+import { Typography } from '@mui/material'
+
 export default function page() {
+  const [productData, setProductData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const productUrl = `${process.env.NEXT_PUBLIC_API_URL_LIVE}/admin/products/allproduct`
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true)
+        const response = await fetchData(productUrl, 'GET')
+        console.log('Get products data', response)
+        setProductData(response)
+      } catch (error) {
+        console.error('error got', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  if (loading) {
+    return <Typography>Wait am loading right now</Typography>
+  }
+  console.log(productData, 'prodddsdfgdfgfdsfghfdghfdg')
   return (
-    <div><Metas/></div>
+    <div>
+      <Allproducts allProductData={productData} />
+    </div>
   )
 }
